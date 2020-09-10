fcf6fe42047872a545d40af727ce9b2079726272
nursaharii <nursahari@hotmail.com>
2020-09-11 00:37:02 +0300

    Fonksiyon overload işlemi

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,5 @@
-var langs;
-langs = ["a", "b", "c"];
-console.log.apply(console, langs);
+//let dizi: string[];
+//dizi=["a","b","c"];
+//console.log(...dizi);
+var sum = function (num1, num2) { return console.log(num1 + num2); }; //Fatarrow
+sum(3, 5);
--- helloworld.ts
+++ helloworld.ts
@@ -2,6 +2,12 @@
 //dizi=["a","b","c"];
 //console.log(...dizi);
 
-let addNumber = (num1: number,num2: number) => console.log(num1+num2);
-
-addNumber(3,5);
+//let sum = (num1: number,num2: number) => console.log(num1+num2);//Fatarrow
+function add(num1: number, num2: number): number;
+function add (num1:string, num2:string):string;
+function add(a: any, b:any): any {
+    return a + b;
+}
+//Fonksiyon overload edebilmek için any ile tanımlama yapmak gerekiyormuş...
+sum(3,5);
+sum("Hello","world");
