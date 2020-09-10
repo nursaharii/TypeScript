8be4ea503fa579256df7ee66e58ad410e13cb09f
nursaharii <nursahari@hotmail.com>
2020-09-11 00:08:45 +0300

    FatArrow ile foksiyon yazılışı

--- helloworld.ts
+++ helloworld.ts
@@ -1,3 +1,7 @@
-let langs: string[];
-langs=["a","b","c"];
-console.log(...langs);
+//let dizi: string[];
+//dizi=["a","b","c"];
+//console.log(...dizi);
+
+let addNumber = (num1: number,num2: number) => console.log(num1+num2);
+
+addNumber(3,5);
