c70ce2e6d142bda188f1afd11dd564dddbfbe835
nursaharii <nursahari@hotmail.com>
2020-09-11 22:50:22 +0300

    Interface, extending, readonly ile iligli bilgiler

--- helloworld.js
+++ helloworld.js
@@ -1,20 +1,10 @@
-//let dizi: string[];
-//dizi=["a","b","c"];
-//console.log(...dizi);
-//let sum = (num1: number,num2: number) => console.log(num1+num2);//Fatarrow
-/*function add(num1: number, num2: number): number;
-function add (num1:string, num2:string):string;
-function add(a: any, b:any): any {
-    return a + b;
-}
-//Fonksiyon overload edebilmek için any ile tanımlama yapmak gerekiyormuş...
-sum(3,5);
-sum("Hello","world");*/
-var Greet = function (greeting) {
-    var names = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        names[_i - 1] = arguments[_i];
-    }
-    return console.log(greeting + " " + names.join(", ") + "!");
-}; //names önündeki 3 nokta sayesinde boş bıraksak da hata ermez
-Greet("Hello"); //buraya 2. parametre yazma zorunluluğum yok (rest parameter)
+var empObj = {
+    empCode: 1,
+    name: "Bill",
+    gender: "Male",
+    age: 15
+};
+empObj.age = "20"; //Üstte bunu readonly tanımladık o yüzden hata verir.
+empObj.empCode = 2;
+empObj.name = "Sofi";
+empObj.gender = "Female";
--- helloworld.ts
+++ helloworld.ts
@@ -1,17 +1,37 @@
-//let dizi: string[];
-//dizi=["a","b","c"];
-//console.log(...dizi);
-
-//let sum = (num1: number,num2: number) => console.log(num1+num2);//Fatarrow
-/*function add(num1: number, num2: number): number;
+/*let dizi: string[];
+dizi=["a","b","c"];
+console.log(...dizi);
+let sum = (num1: number,num2: number) => console.log(num1+num2);//Fatarrow
+function add(num1: number, num2: number): number;
 function add (num1:string, num2:string):string;
 function add(a: any, b:any): any {
     return a + b;
 }
-//Fonksiyon overload edebilmek için any ile tanımlama yapmak gerekiyormuş...
+Fonksiyon overload edebilmek için any ile tanımlama yapmak gerekiyormuş...
 sum(3,5);
-sum("Hello","world");*/
+sum("Hello","world");
 
 let Greet = (greeting: string, ...names: string[]) => console.log(greeting+ " "+ names.join(", ")+"!");//names önündeki 3 nokta sayesinde boş bıraksak da hata ermez
 
-Greet("Hello");//buraya 2. parametre yazma zorunluluğum yok (rest parameter)
+Greet("Hello");//buraya 2. parametre yazma zorunluluğum yok (rest parameter)*/
+interface IPerson {
+    name: string;
+    gender: string;
+    readonly age: number; 
+}
+
+interface IEmployee extends IPerson { //Java gibi burada da bir interface başka bir interface'den miras alabilir.S
+    empCode: number;
+}
+
+let empObj:IEmployee = {
+    empCode:1,
+    name:"Bill",
+    gender:"Male",
+    age: 15
+}
+empObj.age= "20"; //Üstte bunu readonly tanımladık o yüzden hata verir.
+empObj.empCode= 2;
+empObj.name= "Sofi";
+empObj.gender = "Female";
+
